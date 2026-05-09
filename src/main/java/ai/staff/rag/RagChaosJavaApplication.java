09abdf9eaf0050e98344712974952daaa2d7d957
sooraz09 <boddetisooraz@gmail.com>
2026-05-09 11:35:46 +0530

    base commit

---
+++ src/main/java/ai/staff/rag/RagChaosJavaApplication.java
@@ -0,0 +1,13 @@
+package ai.staff.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChaosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagChaosJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/staff/rag/RagChaosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.staff.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChaosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
